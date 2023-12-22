fa8507937046b71b5386dfe8bbcc3974ab55d725
Daniel <dc36205@gmail.com>
2023-12-22 14:54:18 -0800

    Changing plots

--- app.py
+++ app.py
@@ -14,24 +14,24 @@ tab1, tab2 = st.tabs(["Tab1", "Tab2"])
 
 with tab1:
     fig, ax = plt.subplots(1, 2, sharey=True) 
-    ax[0].hist(X, bins=50, aplha=0.5, color="g")
-    ax[0].spines["right"].set_visible(False)
-    ax[0].spines["top"].set_visible(False)    
+    ax[0].hist(X, bins=50, alpha=0.5, color='g')
+    ax[0].spines['right'].set_visible(False)
+    ax[0].spines['top'].set_visible(False)    
     ax[0].axvline(X.mean(), color="g")
 
-    ax[1].hist(Y, bins=50, aplha=0.5, color="g")
-    ax[1].spines["right"].set_visible(False)
-    ax[1].spines["top"].set_visible(False)    
+    ax[1].hist(Y, bins=50, alpha=0.5, color='g')
+    ax[1].spines['right'].set_visible(False)
+    ax[1].spines['top'].set_visible(False)    
     ax[1].axvline(Y.mean(), color="g")
 
     st.pyplot(fig)
 
 with tab2:
     fig, ax = plt.subplots(1, 1) 
-    ax.plot(e, color="k", aplha=0.5, ls = "--")
-    ax.spines["right"].set_visible(False)
-    ax.spines["top"].set_visible(False)
-    ax.axvline(e.mean(), color="g")
+    ax.plot(e, color="k", alpha=0.6, ls = '--')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.axhline(e.mean(), color='k')
 
     st.pyplot(fig)
       
@@ -40,6 +40,3 @@ with tab2:
 
 
 
-st.header('Lanzar una moneda')
-
-st.write('Esta aplicación aún no es funcional, pero estará listaa pronto. En construcción.') 
